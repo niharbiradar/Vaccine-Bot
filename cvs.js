27b891cf88f59243956f4e0d06d8e1f6713f6265
nbiradar86 <niharbiradar@gmail.com>
2022-01-10 15:37:35 -0500

    cvs button click

--- cvs.js
+++ cvs.js
@@ -6,10 +6,13 @@ async function getCVS() {
         defaultViewport: null
     });
     const page = await browser.newPage();
-    const url = 'https://www.cvs.com/vaccine/intake/store/vaccine-select?icid=coronavirus-lp-vaccine-hero-schedule';
+    const url = 'https://www.cvs.com/vaccine/intake/store/covid-screener/covid-qns';
 
     await page.goto(url);    
 
+    await page.click('#q8_1', {clickCount:1});
+    
 }
+
 
 getCVS()
