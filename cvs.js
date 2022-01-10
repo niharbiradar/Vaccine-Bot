ae3b0e81e751a55c80d0ffaa71af2f54f96f5813
nbiradar86 <niharbiradar@gmail.com>
2022-01-09 23:09:30 -0500

    cvs only file

--- cvs.js
+++ cvs.js
@@ -7,11 +7,8 @@ async function getCVS() {
     });
     const page = await browser.newPage();
     const url = 'https://www.cvs.com/vaccine/intake/store/vaccine-select?icid=coronavirus-lp-vaccine-hero-schedule';
-    const pageTwo = await browser.newPage();
-    const urlTwo = 'https://www.walgreens.com/findcare/vaccination/covid/19/landing';
 
     await page.goto(url);
-    await pageTwo.goto(urlTwo);
 }
 
 getCVS()
