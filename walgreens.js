6938a719a948640a0aec92395176740dd8ac53cb
Rohan Desai <rohandesia675@gmail.com>
2022-01-10 15:38:14 -0500

    wal

--- walgreens.js
+++ walgreens.js
@@ -1,2 +1,17 @@
 const puppeteer = require('puppeteer');
 
+async function getWalgreens() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null
+    });
+
+    const url = 'https://www.walgreens.com/findcare/vaccination/covid/19/landing';
+    const pageTwo = await browser.newPage();
+    
+    await pageTwo.goto(url);
+
+    //page click first form
+    await pageTwo.click("#covid-btn", {clickCount:1});
+}
+getWalgreens();
